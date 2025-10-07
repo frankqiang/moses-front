cd536a9430cbb503b03b621fe03e691a944e09ee
Yiqiang Tang <tyq155888@163.com>
2025-10-07 10:12:28 +0800

    fix(constants): 修复常量导出方式
    - 将export default改为export *命名导出
    - 解决14个"export was not found"编译警告
    - 确保所有常量能被正确导入使用

--- src/views/master-data/process-parameter-management/constants/index.js
+++ src/views/master-data/process-parameter-management/constants/index.js
@@ -4,23 +4,27 @@
  * 创建日期：2025-09-29
  * 修改记录：
  *   - 2025-09-29: 初始创建，导出TASK002 P0阶段全部常量配置
+ *   - 2025-09-30: 修复导出方式，使用命名导出而非默认导出
  */
 
-import * as baseConstants from './process-parameter-management'
-import * as tableConfig from './table-config'
-import * as searchConfig from './search-config'
-import * as templateFormConfig from './template-form-config'
-import * as versionFormConfig from './version-form-config'
-import * as messagesConfig from './messages-config'
-import * as chartConfig from './chart-config'
+// 基础常量
+export * from './process-parameter-management'
 
-export default {
-  ...baseConstants,
-  ...tableConfig,
-  ...searchConfig,
-  ...templateFormConfig,
-  ...versionFormConfig,
-  ...messagesConfig,
-  ...chartConfig
-}
+// 表格配置
+export * from './table-config'
+
+// 搜索配置
+export * from './search-config'
+
+// 模板表单配置
+export * from './template-form-config'
+
+// 版本表单配置
+export * from './version-form-config'
+
+// 消息配置
+export * from './messages-config'
+
+// 图表配置
+export * from './chart-config'
 
