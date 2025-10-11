da088705f572995dd3dc197851b49c6cbe16c9d3
Yiqiang Tang <tyq155888@163.com>
2025-10-11 09:10:07 +0800

    fix(inventory): 修复料框管理API导出文件的路径引用错误
    - 移除对不存在的./stack-management的引用
    - bin-management和stack-management是独立模块，各自维护自己的API

--- src/views/inventory-management/bin-management/api/index.js
+++ src/views/inventory-management/bin-management/api/index.js
@@ -1,9 +1,9 @@
 /**
  * 文件名称：index.js
- * 文件描述：料框/料垛管理API统一导出入口
+ * 文件描述：料框管理API统一导出入口
  * 创建日期：2025-01-10
  * 修改记录:
- *   - 2025-01-10: 初始创建，统一导出料框和料垛管理接口
+ *   - 2025-01-10: 初始创建，统一导出料框管理接口
  */
 
 // 料框管理接口
@@ -15,12 +15,3 @@ export {
   updateBinStatus,
   getBinStatusHistory
 } from './bin-management'
-
-// 料垛管理接口
-export {
-  createStack,
-  getStackList,
-  getStackDetail,
-  destackStack,
-  getStackBins
-} from './stack-management'
